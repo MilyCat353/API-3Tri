a084a50a02d5bf9c651835c3118b21f038c94839
MilyCat353 <146204164+MilyCat353@users.noreply.github.com>
2025-08-20 11:34:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const db = require("./db")
 app.use(express.json())
 
 const usuarios = []
-app.post('/usuarios', (req, res)=>{
+app.post('/usuarios', async(req, res)=>{
     const usuario = req.body
     usuarios.push(usuario)
+    try{
+        const result =  await db.pool.query('INSERT INTO cliente (nome); VALUES (?)', [usuario.nome])
+    
     res.send("Usuario cadastrado com sucesso")
+    } catch (err){
+        throw err;
+    }
 })
 app.get('/usuarios', (req, res)=>{
     res.send(usuarios)
+
 })
 
 
